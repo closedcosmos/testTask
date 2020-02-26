aa6d352bd0866ec14d5a630342b14362b6a980ef
Vitalina Vezhnavets <vvezhn@gmail.com>
2020-02-26 16:38:35 +0200

    fix mobile view

--- src/Components/form.js
+++ src/Components/form.js
@@ -11,20 +11,20 @@ class Form extends Component {
                 <Section collapsible={false} title={'Test form'}>
                     <Section form={true} title={'Ehitise üldinfo'}>
                         <div className="form">
-                            <div className="form-line">
-                                <div></div>
-                                <div></div>
-                                <div></div>
-                                <div><Readonly value={'Taotluse andmed'} /></div>
-                                <div><Readonly value={'EHR andmed'} /></div>
-                                <div></div>
+                            <div className="form-row form-header">
+                                <div className="col-sm-20 col-md-4 col-lg-4"></div>
+                                <div className="col-sm-20 col-md-6 col-lg-6"><Readonly value={'Taotluse andmed'} /></div>
+                                <div className="col-sm-20 col-md-2 col-lg-2"><Readonly value={'EHR andmed'} /></div>
+                                <div className="col-sm-20 col-md-4 col-lg-4"></div>
                             </div>
                             {Data.map((item, index) => {
                                 return (
-                                    <div className="form-line" key={index}>
-                                        <div><Readonly value={item.name} /></div>
-                                        <div><Important item={item} /></div>
-                                        <div><Info tooltipText={item.info} /></div>
+                                    <div className="form-row" key={index}>
+                                        <div className="col-sm-20 col-md-4 col-lg-4 right inline">
+                                            <div><Readonly value={item.name} /></div>
+                                            <div><Important item={item} /></div>
+                                            <div><Info tooltipText={item.info} /></div>
+                                        </div>
                                         <Value item={item}/>
                                     </div>
                                 )
--- src/Components/value.js
+++ src/Components/value.js
@@ -1,35 +1,40 @@
 import React, {Component} from 'react';
-import {Readonly, Select, Number, DatePicker, Checkbox, Input} from 'mta-react-common-visuals';
+import {Readonly, Select, Number, DatePicker, Checkbox, Input, Info} from 'mta-react-common-visuals';
 
 class Value extends Component {
     render() {
         const {item} = this.props;
-        var thirdColumn, fourthColumn, fifthColumn = null;
+        var secondColumn, thirdColumn, fourthColumn = null;
         if (item.type === "read") {
-            thirdColumn = <Readonly value={item.value} />
+            secondColumn = <Readonly value={item.value} />
         } else if (item.type === "input") {
-            thirdColumn = <Input name={'input'} onChange={value => console.log(value)} />
-            fourthColumn = <Readonly value={item.ehr} />
+            secondColumn = <Input name={'input'} onChange={value => console.log(value)} />
+            thirdColumn = <Readonly value={item.ehr} />
         } else if (item.type === "select") {
-            thirdColumn = <Select name={'select'} options={item.options} onChange={value => console.log(value)} />
-            fourthColumn = <Readonly value={item.ehr} />
+            secondColumn = <Select name={'select'} options={item.options} onChange={value => console.log(value)} />
+            thirdColumn = <Readonly value={item.ehr} />
         } else if (item.type === "number") {
-            thirdColumn = <Number name={'number'} onChange={value => console.log(value)} />
-            fourthColumn = <Readonly value={item.ehr} />
-            fifthColumn = <div><Checkbox info={{content: item.subField}} name={'checkbox'} onChange={value => console.log(value)} text={item.subField} /></div>
+            secondColumn = <Number name={'number'} onChange={value => console.log(value)} />
+            thirdColumn = <Readonly value={item.ehr} />
+            fourthColumn = <><Checkbox name={'checkbox'} onChange={value => console.log(value)} />
+                            <Info tooltipText={item.subField} />
+                            <Readonly value={item.subField} />
+                            </>
         } else if (item.type === "date") {
-            thirdColumn = <DatePicker onChange={value => console.log(value)}/>
+            secondColumn = <DatePicker onChange={value => console.log(value)}/>
         }
 
         return (
             <>
-                <div>
+                <div className="col-sm-20 col-md-6 col-lg-6">
+                    {secondColumn}
+                </div>
+                <div className="col-sm-20 col-md-2 col-lg-2 gray">
                     {thirdColumn}
                 </div>
-                <div>
+                <div className="col-sm-20 col-md-4 col-lg-4 inline">
                     {fourthColumn}
                 </div>
-                {fifthColumn}
             </>
         )
     }
